2ee2b64dce604f57abe42d090036fe728d658a61
Yosuke Otosu <yosuke.otosu@gmail.com>
2019-12-10 22:35:04 +0900

    fix huobi

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/fxpgr/go-exchange-client/api/private"
-	"github.com/fxpgr/go-exchange-client/api/public"
-)
-
 func main() {
-	cli, err := public.NewClient("huobi")
+	/*cli, err := public.NewClient("huobi")
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
@@ -17,11 +10,5 @@ func main() {
 	fmt.Println(cli.Volume("ZRX", "BTC"))
 	fmt.Println(cli.Board("ZRX", "BTC"))
 	fmt.Println(cli.OrderBookTickMap())
-	fmt.Println(cli.Precise("ZRX", "BTC"))
-	pcli, err := private.NewClient(private.PROJECT, "huobi", func() (string, error) { return "", nil }, func() (string, error) { return "", nil })
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-	fmt.Println(pcli.TransferFee())
+	fmt.Println(cli.Precise("ZRX", "BTC"))*/
 }
